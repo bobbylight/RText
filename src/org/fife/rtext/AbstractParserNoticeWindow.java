ca6d76155ae1508122871caf24b8a131f8bf4f7d
bobbylight <robert@fifesoft.com>
2009-10-22 03:35:28 +0000

    Workaround for Sun bug 5049957 (parser notice windows would throw NPE when changing from Windows LaF to another LaF).

--- src/org/fife/rtext/AbstractParserNoticeWindow.java
+++ src/org/fife/rtext/AbstractParserNoticeWindow.java
@@ -1,3 +1,26 @@
+/*
+ * 10/21/2009
+ *
+ * AbstractParserNoticeWindow.java - Base class for dockable windows that
+ * display parser notices.
+ * Copyright (C) 2009 Robert Futrell
+ * robert_futrell at users.sourceforge.net
+ * http://fifesoft.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package org.fife.rtext;
 
 import java.awt.Component;
@@ -7,16 +30,20 @@ import java.util.Comparator;
 import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.Icon;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.UIManager;
 import javax.swing.border.Border;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableModel;
 import javax.swing.text.BadLocationException;
 
 import org.fife.ui.FileExplorerTableModel;
+import org.fife.ui.FileExplorerTableModel.SortableHeaderRenderer;
 import org.fife.ui.dockablewindows.DockableWindow;
 
 
@@ -38,10 +65,20 @@ abstract class AbstractParserNoticeWindow extends DockableWindow {
 
 
 	protected JTable createTable(TableModel model) {
-
 		table = new JTable();
+		fixTableModel(model);
+		table.addMouseListener(new TableMouseListener());
+		return table;
+	}
+
+
+	private void fixTableModel(TableModel model) {
+
+		JTableHeader old = table.getTableHeader();
+		table.setTableHeader(new JTableHeader(old.getColumnModel()));
+
 		FileExplorerTableModel model2 = new FileExplorerTableModel(model,
-													table.getTableHeader());
+				table.getTableHeader());
 
 		model2.setColumnComparator(Integer.class, new Comparator() {	
 			public int compare(Object o1, Object o2) {
@@ -61,7 +98,7 @@ abstract class AbstractParserNoticeWindow extends DockableWindow {
 
 		table.setDefaultRenderer(Icon.class, new IconTableCellRenderer());
 		table.setDefaultRenderer(RTextEditorPane.class,
-				new TextAreaTableCellRenderer());
+								new TextAreaTableCellRenderer());
 
 		table.setModel(model2);
 
@@ -75,9 +112,56 @@ abstract class AbstractParserNoticeWindow extends DockableWindow {
 		tcm.getColumn(3).setPreferredWidth(800);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
-		table.addMouseListener(new TableMouseListener());
+	}
 
-		return table;
+
+	/**
+	 * Overridden to work around Java bug 6429812.  As of Java 6 (!), changing
+	 * LaF from Windows to something else (e.g. Metal) at runtime with a custom
+	 * delegating TableCellRenderer on a JTableHeader can cause NPE's.  Then,
+	 * the UI never repaints from then on.  Seems to happen only when the table
+	 * is actually visible during the LaF change.  See
+	 * <a href="http://bugs.sun.com/view_bug.do?bug_id=6429812">6429812</a>.
+	 */
+	public void updateUI() {
+
+		/*
+		 * NOTE: This is silly, but it's what it took to get a LaF change to
+		 * occur without throwing an NPE because of the JRE bug.  No doubt there
+		 * is a better way to handle this.  What we do is:
+		 *
+		 * 1. Before updating the UI, reset the JTableHeader's default renderer
+		 *    to what it was originally.  This prevents the NPE from the JRE
+		 *    bug, since we're no longer using the renderer with a cached
+		 *    Windows-specific TableCellRenderer (when Windows LaF is enabled).
+		 * 2. Update the UI, like normal.
+		 * 3. After the update, we must explicitly re-set the JTableHeader as
+		 *    the column view in the enclosing JScrollPane.  This is done by
+		 *    default the first time you add a JTable to a JScrollPane, but
+		 *    since we gave the JTable a new header as a workaround for the JRE
+		 *    bug, we must explicitly tell the JScrollPane about it as well.
+		 */
+
+		TableModel model = null;
+		if (table!=null) {
+			model = ((FileExplorerTableModel)table.getModel()).getTableModel();
+			TableCellRenderer r = table.getTableHeader().getDefaultRenderer();
+			if (r instanceof SortableHeaderRenderer) { // Always true
+				SortableHeaderRenderer shr = (SortableHeaderRenderer)r;
+				table.getTableHeader().setDefaultRenderer(
+											shr.getDelegateRenderer());
+			}
+		}
+
+		super.updateUI();
+
+		if (table!=null) {
+			JScrollPane sp = (JScrollPane)table.getParent().getParent();
+			fixTableModel(model);
+			sp.setColumnHeaderView(table.getTableHeader());
+			sp.revalidate();
+			sp.repaint();
+		}
 
 	}
 
